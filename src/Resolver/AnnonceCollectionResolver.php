99e626f379af8683bcc9e8d931f7cc67cfe9e797
Mohammad Trabelsi <mohammad.trabelsi@foodomarket.com>
2022-04-11 09:50:42 +0100

    work with annopnces [WIP]

---
+++ src/Resolver/AnnonceCollectionResolver.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Resolver;
+
+use ApiPlatform\Core\GraphQl\Resolver\QueryCollectionResolverInterface;
+
+
+final  class CategoryCollectionResolver implements QueryCollectionResolverInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function __invoke(iterable $collection, array $context): iterable
+    {
+        // Query arguments are in $context['args'].
+        dd($context['args']);
+        foreach ($collection as $category) {
+            $category->setName("name");
+            $category->setSlug("slug");
+        }
+        return $collection;
+    }
+}
